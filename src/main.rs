7fc73f0a927767ca48acacc14ad44d47f79eba08
Viktor Berezovoi <vvb_pro@outlook.com>
2026-07-25 13:53:09 +0700

    use DB with PgPool

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
 use actix_hello::{configuration::get_configuration, startup::run};
+use sqlx::{Connection, PgPool};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -8,7 +9,10 @@ async fn main() -> std::io::Result<()> {
         Ok(value) => value,
         Err(e) => panic!("Failed to load configuration {e}"),
     };
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
